b947008086bc8179da23a6cbbcf9869c2d774378
= <=>
2021-04-24 08:08:41 +0100

    added  tailwind to index page

---
+++ blog.dev.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+	enabled: false,
+	content: ['*.html']
+	},
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
